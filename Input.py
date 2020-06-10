da34694aa18086d00e10da4f0a97bcc308052d3d
TrongDT2 <65997915+TrongDT2@users.noreply.github.com>
2020-06-10 14:35:00 +0700

    Update Input.py

--- Input.py
+++ Input.py
@@ -1,1 +1,381 @@
-import pprintpp = pprint.PrettyPrinter(indent=4)webtech=[{  'icon': 'jQuery.svg',  'name': 'jQuery',  'applications': [{    'icon': 'jQuery.svg',    'port': '80',    'website': 'https://jquery.com',    'confidence': 100,    'version': '3.3.1',    'path': '/',    'categories': ['JavaScript Libraries']  }, {    'icon': 'jQuery.svg',    'port': '80',    'website': 'https://jquery.com',    'confidence': 100,    'version': '3.3.3',    'path': '/index.html',    'categories': ['JavaScript Libraries']  }, {    'icon': 'jQuery.svg',    'port': '443',    'website': 'https://jquery.com',    'confidence': 100,    'version': '3.3.1',    'path': '/',    'categories': ['JavaScript Libraries']  }, {    'icon': 'jQuery.svg',    'port': '443',    'website': 'https://jquery.com',    'confidence': 100,    'version': '3.3.1.2',    'path': '/index.html',    'categories': ['JavaScript Libraries']  }]#1}, {  'icon': 'Apache.svg',  'name': 'Apache',  'applications': [{    'icon': 'Apache.svg',    'port': '80',    'website': 'http://apache.org',    'confidence': 100,    'version': '2.4.6',    'path': '/',    'categories': ['Web Servers']  }, {    'icon': 'Apache.svg',    'port': '80',    'website': 'http://apache.org',    'confidence': 100,    'version': '2.4.6',    'path': '/index.html',    'categories': ['Web Servers']  }, {    'icon': 'Apache.svg',    'port': '443',    'website': 'http://apache.org',    'confidence': 100,    'version': '2.4.6',    'path': '/',    'categories': ['Web Servers']  }, {    'icon': 'Apache.svg',    'port': '443',    'website': 'http://apache.org',    'confidence': 100,    'version': '2.4.6',    'path': '/index.html',    'categories': ['Web Servers']  }]  #2}, {  'icon': 'Underscore.js.png',  'name': 'Underscore.js',  'applications': [{    'icon': 'Underscore.js.png',    'port': '80',    'website': 'http://underscorejs.org',    'confidence': 100,    'version': '1.8.3',    'path': '/',    'categories': ['JavaScript Libraries']  }, {    'icon': 'Underscore.js.png',    'port': '80',    'website': 'http://underscorejs.org',    'confidence': 100,    'version': '1.8.3',    'path': '/index.html',    'categories': ['JavaScript Libraries']  }, {    'icon': 'Underscore.js.png',    'port': '443',    'website': 'http://underscorejs.org',    'confidence': 100,    'version': '1.8.3',    'path': '/',    'categories': ['JavaScript Libraries']  }, {    'icon': 'Underscore.js.png',    'port': '443',    'website': 'http://underscorejs.org',    'confidence': 100,    'version': '1.8.3',    'path': '/index.html',    'categories': ['JavaScript Libraries']  }]  #3}, {  'icon': 'PHP.svg',  'name': 'PHP',  'applications': [{    'icon': 'PHP.svg',    'port': '80',    'website': 'http://php.net',    'confidence': 100,    'version': '7.2.16',    'path': '/',    'categories': ['Programming Languages']  }, {    'icon': 'PHP.svg',    'port': '80',    'website': 'http://php.net',    'confidence': 100,    'version': '7.2.16',    'path': '/index.html',    'categories': ['Programming Languages']  }, {    'icon': 'PHP.svg',    'port': '443',    'website': 'http://php.net',    'confidence': 100,    'version': '7.2.16',    'path': '/',    'categories': ['Programming Languages']  }, {    'icon': 'PHP.svg',    'port': '443',    'website': 'http://php.net',    'confidence': 100,    'version': '7.2.16',    'path': '/index.html',    'categories': ['Programming Languages']  }]  #4}, {  'icon': 'CentOS.png',  'name': 'CentOS',  'applications': [{    'icon': 'CentOS.png',    'port': '80',    'website': 'http://centos.org',    'confidence': 100,    'version': None,    'path': '/',    'categories': ['Operating Systems']  }, {    'icon': 'CentOS.png',    'port': '80',    'website': 'http://centos.org',    'confidence': 100,    'version': None,    'path': '/index.html',    'categories': ['Operating Systems']  }, {    'icon': 'CentOS.png',    'port': '443',    'website': 'http://centos.org',    'confidence': 100,    'version': None,    'path': '/',    'categories': ['Operating Systems']  }, {    'icon': 'CentOS.png',    'port': '443',    'website': 'http://centos.org',    'confidence': 100,    'version': None,    'path': '/index.html',    'categories': ['Operating Systems']  }]  #5}, {  'icon': 'OpenSSL.png',  'name': 'OpenSSL',  'applications': [{    'icon': 'OpenSSL.png',    'port': '80',    'website': 'http://openssl.org',    'confidence': 100,    'version': '1.0.2k',    'path': '/',    'categories': ['Web Server Extensions']  }, {    'icon': 'OpenSSL.png',    'port': '80',    'website': 'http://openssl.org',    'confidence': 100,    'version': '1.0.2k',    'path': '/index.html',    'categories': ['Web Server Extensions']  }, {    'icon': 'OpenSSL.png',    'port': '443',    'website': 'http://openssl.org',    'confidence': 100,    'version': '1.0.2k',    'path': '/',    'categories': ['Web Server Extensions']  }, {    'icon': 'OpenSSL.png',    'port': '443',    'website': 'http://openssl.org',    'confidence': 100,    'version': '1.0.2k',    'path': '/index.html',    'categories': ['Web Server Extensions']  }]  #6}, {  'icon': 'jQuery UI.svg',  'name': 'jQuery UI',  'applications': [{    'icon': 'jQuery UI.svg',    'port': '80',    'website': 'http://jqueryui.com',    'confidence': 100,    'version': '1.12.1',    'path': '/',    'categories': ['JavaScript Libraries']  }, {    'icon': 'jQuery UI.svg',    'port': '80',    'website': 'http://jqueryui.com',    'confidence': 100,    'version': '1.12.1',    'path': '/index.html',    'categories': ['JavaScript Libraries']  }, {    'icon': 'jQuery UI.svg',    'port': '443',    'website': 'http://jqueryui.com',    'confidence': 100,    'version': '1.12.1',    'path': '/',    'categories': ['JavaScript Libraries']  }, {    'icon': 'jQuery UI.svg',    'port': '443',    'website': 'http://jqueryui.com',    'confidence': 100,    'version': '1.12.1',    'path': '/index.html',    'categories': ['JavaScript Libraries']  }]}]#print(a[0]['applications'][0]['version'])#list a        """Groupby = []for i in a:  #i la tung vi tri trong list a (tung cai dict)    # dict a[i][application]    for i2 in i['applications']: #i2 la tung cai dic cua value 'application'             k = {'version' : i2['version'], 'port' : i2['port'], 'path' : i2['path']}          for i3 in Groupby:          a = [i3['port']] ##dang co van de                if i2['version'] == i3['version']:            if i3['port'] != i2['port']:              a.append(i2['port'])              i3['port'] = a        Groupby.append(k)for i in Groupby:   print(i)"""# a2=[]# groupby=[]# for i in a:#   # tao dic trong a2#   adddic = {'icon':i['icon'] , 'name':i['name'] , 'applications':i['applications'] , 'groupBy':groupby}#   a2.append(adddic)#   #dung de tim version trong applcation tai i2#   for i2 in i['applications']:#       gb=[]#       if i2['version'] in gb:#         continue#       else:#         gb.append({'version':i2['version'], 'port':[], 'path':[]})#       groupby = gb# print()"""Groupby = []for i in a: for i2 in i['applications']:   if i2['version'] in Groupby:     continue   else:     Groupby.append({'version' : i2['version'], 'port' : [], 'path' : []})for x in a: for x2 in x['applications']:   for x3 in Groupby:     if x2['version'] == x3['version']:       if x2['port'] not in x3['port']:         x3['port'].append(x2['port'])       if x2['path'] not in x3['path']:         x3['path'].append(x2['path'])for x in Groupby: print(x) pass"""lliii=[]# li=[{'version':44,'leu leu':'kk'}]# for i in li:#   if i['version'] not in lliii:#     lliii.append(i['version']) # print('alo')# print(lliii)# for tech in webtech:#   for dataInList in li:#     for dataIndex in tech['applications']:#       if dataIndex['version'] in dataInList['version']:#         continue#       else:#         li.append({'ai ma biet': dataIndex['version']})for tech in webtech:  lis=[]  for dataIndex in tech['applications']:    # pass    # print(dataIndex)    # print(type(dataIndex))    #for dataInList in dataIndex:    #check version      if dataIndex['version'] in dataIndex:        continue      else:        #add version not in lis to lis        lis.append({'version' : dataIndex['version'], 'port' : [], 'path' : []})             #add port and path      for dataGB in lis:       if dataIndex['version'] == dataGB['version']:         if dataIndex['port'] not in dataGB['port']:           dataGB['port'].append(dataIndex['port'])         if dataIndex['path'] not in dataGB['path']:           dataGB['path'].append(dataIndex['path'])  tech['groupby'] = lispp.pprint(webtech)
+import pprint
+pp = pprint.PrettyPrinter(indent=4)
+webtech=[{
+  'icon': 'jQuery.svg',
+  'name': 'jQuery',
+  'applications': [{
+    'icon': 'jQuery.svg',
+    'port': '80',
+    'website': 'https://jquery.com',
+    'confidence': 100,
+    'version': '3.3.1',
+    'path': '/',
+    'categories': ['JavaScript Libraries']
+  }, {
+    'icon': 'jQuery.svg',
+    'port': '80',
+    'website': 'https://jquery.com',
+    'confidence': 100,
+    'version': '3.3.3',
+    'path': '/index.html',
+    'categories': ['JavaScript Libraries']
+  }, {
+    'icon': 'jQuery.svg',
+    'port': '443',
+    'website': 'https://jquery.com',
+    'confidence': 100,
+    'version': '3.3.1',
+    'path': '/',
+    'categories': ['JavaScript Libraries']
+  }, {
+    'icon': 'jQuery.svg',
+    'port': '443',
+    'website': 'https://jquery.com',
+    'confidence': 100,
+    'version': '3.3.1.2',
+    'path': '/index.html',
+    'categories': ['JavaScript Libraries']
+  }]
+#1
+}, {
+  'icon': 'Apache.svg',
+  'name': 'Apache',
+  'applications': [{
+    'icon': 'Apache.svg',
+    'port': '80',
+    'website': 'http://apache.org',
+    'confidence': 100,
+    'version': '2.4.6',
+    'path': '/',
+    'categories': ['Web Servers']
+  }, {
+    'icon': 'Apache.svg',
+    'port': '80',
+    'website': 'http://apache.org',
+    'confidence': 100,
+    'version': '2.4.6',
+    'path': '/index.html',
+    'categories': ['Web Servers']
+  }, {
+    'icon': 'Apache.svg',
+    'port': '443',
+    'website': 'http://apache.org',
+    'confidence': 100,
+    'version': '2.4.6',
+    'path': '/',
+    'categories': ['Web Servers']
+  }, {
+    'icon': 'Apache.svg',
+    'port': '443',
+    'website': 'http://apache.org',
+    'confidence': 100,
+    'version': '2.4.6',
+    'path': '/index.html',
+    'categories': ['Web Servers']
+  }]
+  #2
+}, {
+  'icon': 'Underscore.js.png',
+  'name': 'Underscore.js',
+  'applications': [{
+    'icon': 'Underscore.js.png',
+    'port': '80',
+    'website': 'http://underscorejs.org',
+    'confidence': 100,
+    'version': '1.8.3',
+    'path': '/',
+    'categories': ['JavaScript Libraries']
+  }, {
+    'icon': 'Underscore.js.png',
+    'port': '80',
+    'website': 'http://underscorejs.org',
+    'confidence': 100,
+    'version': '1.8.3',
+    'path': '/index.html',
+    'categories': ['JavaScript Libraries']
+  }, {
+    'icon': 'Underscore.js.png',
+    'port': '443',
+    'website': 'http://underscorejs.org',
+    'confidence': 100,
+    'version': '1.8.3',
+    'path': '/',
+    'categories': ['JavaScript Libraries']
+  }, {
+    'icon': 'Underscore.js.png',
+    'port': '443',
+    'website': 'http://underscorejs.org',
+    'confidence': 100,
+    'version': '1.8.3',
+    'path': '/index.html',
+    'categories': ['JavaScript Libraries']
+  }]
+  #3
+}, {
+  'icon': 'PHP.svg',
+  'name': 'PHP',
+  'applications': [{
+    'icon': 'PHP.svg',
+    'port': '80',
+    'website': 'http://php.net',
+    'confidence': 100,
+    'version': '7.2.16',
+    'path': '/',
+    'categories': ['Programming Languages']
+  }, {
+    'icon': 'PHP.svg',
+    'port': '80',
+    'website': 'http://php.net',
+    'confidence': 100,
+    'version': '7.2.16',
+    'path': '/index.html',
+    'categories': ['Programming Languages']
+  }, {
+    'icon': 'PHP.svg',
+    'port': '443',
+    'website': 'http://php.net',
+    'confidence': 100,
+    'version': '7.2.16',
+    'path': '/',
+    'categories': ['Programming Languages']
+  }, {
+    'icon': 'PHP.svg',
+    'port': '443',
+    'website': 'http://php.net',
+    'confidence': 100,
+    'version': '7.2.16',
+    'path': '/index.html',
+    'categories': ['Programming Languages']
+  }]
+  #4
+}, {
+  'icon': 'CentOS.png',
+  'name': 'CentOS',
+  'applications': [{
+    'icon': 'CentOS.png',
+    'port': '80',
+    'website': 'http://centos.org',
+    'confidence': 100,
+    'version': None,
+    'path': '/',
+    'categories': ['Operating Systems']
+  }, {
+    'icon': 'CentOS.png',
+    'port': '80',
+    'website': 'http://centos.org',
+    'confidence': 100,
+    'version': None,
+    'path': '/index.html',
+    'categories': ['Operating Systems']
+  }, {
+    'icon': 'CentOS.png',
+    'port': '443',
+    'website': 'http://centos.org',
+    'confidence': 100,
+    'version': None,
+    'path': '/',
+    'categories': ['Operating Systems']
+  }, {
+    'icon': 'CentOS.png',
+    'port': '443',
+    'website': 'http://centos.org',
+    'confidence': 100,
+    'version': None,
+    'path': '/index.html',
+    'categories': ['Operating Systems']
+  }]
+  #5
+}, {
+  'icon': 'OpenSSL.png',
+  'name': 'OpenSSL',
+  'applications': [{
+    'icon': 'OpenSSL.png',
+    'port': '80',
+    'website': 'http://openssl.org',
+    'confidence': 100,
+    'version': '1.0.2k',
+    'path': '/',
+    'categories': ['Web Server Extensions']
+  }, {
+    'icon': 'OpenSSL.png',
+    'port': '80',
+    'website': 'http://openssl.org',
+    'confidence': 100,
+    'version': '1.0.2k',
+    'path': '/index.html',
+    'categories': ['Web Server Extensions']
+  }, {
+    'icon': 'OpenSSL.png',
+    'port': '443',
+    'website': 'http://openssl.org',
+    'confidence': 100,
+    'version': '1.0.2k',
+    'path': '/',
+    'categories': ['Web Server Extensions']
+  }, {
+    'icon': 'OpenSSL.png',
+    'port': '443',
+    'website': 'http://openssl.org',
+    'confidence': 100,
+    'version': '1.0.2k',
+    'path': '/index.html',
+    'categories': ['Web Server Extensions']
+  }]
+  #6
+}, {
+  'icon': 'jQuery UI.svg',
+  'name': 'jQuery UI',
+  'applications': [{
+    'icon': 'jQuery UI.svg',
+    'port': '80',
+    'website': 'http://jqueryui.com',
+    'confidence': 100,
+    'version': '1.12.1',
+    'path': '/',
+    'categories': ['JavaScript Libraries']
+  }, {
+    'icon': 'jQuery UI.svg',
+    'port': '80',
+    'website': 'http://jqueryui.com',
+    'confidence': 100,
+    'version': '1.12.1',
+    'path': '/index.html',
+    'categories': ['JavaScript Libraries']
+  }, {
+    'icon': 'jQuery UI.svg',
+    'port': '443',
+    'website': 'http://jqueryui.com',
+    'confidence': 100,
+    'version': '1.12.1',
+    'path': '/',
+    'categories': ['JavaScript Libraries']
+  }, {
+    'icon': 'jQuery UI.svg',
+    'port': '443',
+    'website': 'http://jqueryui.com',
+    'confidence': 100,
+    'version': '1.12.1',
+    'path': '/index.html',
+    'categories': ['JavaScript Libraries']
+  }]
+}]
+
+
+
+#print(a[0]['applications'][0]['version'])
+#list a
+
+
+        
+"""Groupby = []
+for i in a:  #i la tung vi tri trong list a (tung cai dict)
+    # dict a[i][application]
+    for i2 in i['applications']: #i2 la tung cai dic cua value 'application'     
+        k = {'version' : i2['version'], 'port' : i2['port'], 'path' : i2['path']}  
+        for i3 in Groupby:
+          a = [i3['port']] ##dang co van de      
+          if i2['version'] == i3['version']:
+            if i3['port'] != i2['port']:
+              a.append(i2['port'])
+              i3['port'] = a
+        Groupby.append(k)
+
+for i in Groupby:
+   print(i)
+"""
+
+
+
+# a2=[]
+# groupby=[]
+# for i in a:
+#   # tao dic trong a2
+#   adddic = {'icon':i['icon'] , 'name':i['name'] , 'applications':i['applications'] , 'groupBy':groupby}
+#   a2.append(adddic)
+#   #dung de tim version trong applcation tai i2
+#   for i2 in i['applications']:
+#       gb=[]
+#       if i2['version'] in gb:
+#         continue
+#       else:
+#         gb.append({'version':i2['version'], 'port':[], 'path':[]})
+#       groupby = gb
+
+# print()
+
+
+
+
+"""
+Groupby = []
+for i in a:
+ for i2 in i['applications']:
+   if i2['version'] in Groupby:
+     continue
+   else:
+     Groupby.append({'version' : i2['version'], 'port' : [], 'path' : []})
+
+for x in a:
+ for x2 in x['applications']:
+   for x3 in Groupby:
+     if x2['version'] == x3['version']:
+       if x2['port'] not in x3['port']:
+         x3['port'].append(x2['port'])
+       if x2['path'] not in x3['path']:
+         x3['path'].append(x2['path'])
+
+for x in Groupby:
+ print(x)
+ pass"""
+lliii=[]
+
+# li=[{'version':44,'leu leu':'kk'}]
+# for i in li:
+#   if i['version'] not in lliii:
+#     lliii.append(i['version']) 
+# print('alo')
+# print(lliii)
+
+
+# for tech in webtech:
+#   for dataInList in li:
+
+#     for dataIndex in tech['applications']:
+#       if dataIndex['version'] in dataInList['version']:
+#         continue
+#       else:
+#         li.append({'ai ma biet': dataIndex['version']})
+
+
+
+for tech in webtech:
+  lis=[]
+  for dataIndex in tech['applications']:
+    # pass
+    # print(dataIndex)
+    # print(type(dataIndex))
+    #for dataInList in dataIndex:
+    #check version
+      if dataIndex['version'] in dataIndex:
+        continue
+      else:
+        #add version not in lis to lis
+        lis.append({'version' : dataIndex['version'], 'port' : [], 'path' : []}) 
+      
+      #add port and path
+      for dataGB in lis:
+       if dataIndex['version'] == dataGB['version']:
+         if dataIndex['port'] not in dataGB['port']:
+           dataGB['port'].append(dataIndex['port'])
+         if dataIndex['path'] not in dataGB['path']:
+           dataGB['path'].append(dataIndex['path'])
+
+
+
+  tech['groupby'] = lis
+
+pp.pprint(webtech)
+
+anhht44
+
+
